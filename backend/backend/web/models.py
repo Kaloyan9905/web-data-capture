b55dd00b609e2cdc78937571eba47ee1ff875b39
kaloyan <milchevkaloian@gmail.com>
2024-04-26 21:22:09 +0300

    new model for saving coordinates and web cam image

--- backend/backend/web/models.py
+++ backend/backend/web/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Model(models.Model):
+    coordinate_x = models.CharField(max_length=4)
+    coordinate_y = models.CharField(max_length=4)
+    image = models.ImageField()
