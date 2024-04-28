cf0247f78e307d30d2e3f98c652caa3be0016276
kaloyan <milchevkaloian@gmail.com>
2024-04-28 19:10:26 +0300

    webcam functionality to capture images

--- backend/backend/web/consumers.py
+++ backend/backend/web/consumers.py
@@ -1,9 +1,14 @@
 import json
+
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+from backend.web.models import Data
+from backend.web.webcam import capture_image
 
 
 class DataConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+    async def connect(self) -> json:
         await self.accept()
         text_data = json.dumps({'message': 'Connection established!'})
         await self.send(text_data)
@@ -11,11 +16,17 @@ class DataConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         pass
 
-    async def receive(self, text_data):
+    async def receive(self, text_data) -> json:
         data_json = json.loads(text_data)
+
         clicked_x = data_json['clicked_x']
         clicked_y = data_json['clicked_y']
-        print(clicked_x)
-        print(clicked_y)
-        # TODO: save the data into a model with web cam image!
-        await self.send(text_data=json.dumps({'message': 'received coordinates'}))
+        image_url = await capture_image()
+
+        await sync_to_async(Data.objects.create)(
+            coordinate_x=clicked_x,
+            coordinate_y=clicked_y,
+            image=image_url,
+        )
+
+        await self.send(text_data=json.dumps({'image_url': image_url}))
--- backend/backend/web/webcam.py
+++ backend/backend/web/webcam.py
@@ -2,6 +2,8 @@ import random
 import cv2
 import os
 
+from django.conf import settings
+
 
 async def capture_image() -> str:
     cap = cv2.VideoCapture(0)
@@ -15,12 +17,11 @@ async def capture_image() -> str:
     if not ret:
         return 'Error: Unable to capture frame'
 
-    # Generate a random filename
     filename = f'webcam_image_{random.randint(1, 100000)}.jpg'
 
     # Save the captured frame
-    save_dir = '../../media'
-    file_path = os.path.normpath(os.path.join(save_dir, filename))
+    save_dir = os.path.join(settings.MEDIA_ROOT, 'webcam_images')
+    file_path = os.path.join(save_dir, filename)
 
     cv2.imwrite(file_path, frame)
     cap.release()
