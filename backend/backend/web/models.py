1ac03187ee310d4f07bc96f2982fb670611ed98f
kaloyan <milchevkaloian@gmail.com>
2024-04-28 19:11:50 +0300

    added upload path and renamed model to Data

--- backend/backend/web/models.py
+++ backend/backend/web/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Model(models.Model):
+class Data(models.Model):
     coordinate_x = models.CharField(max_length=4)
     coordinate_y = models.CharField(max_length=4)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='media/')
